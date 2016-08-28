caa8dec9e72a0f041789e7174a338c38f3f71f10
Joel Helbling <joel@joelhelbling.com>
2016-08-27 22:21:05 -0400

    install nvm

--- lib/initial_server_setup.sh
+++ lib/initial_server_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#<UDF name="USERNAME" label="main user login" default="fooser" description="A non-root (but sudoing) user for the server" />
-#<UDF name="GITHUB_USER" label="GitHub user whose keys will be installed" default="" description="Optional GitHub user for authorized_keys" />
-#<UDF name="TIMEZONE" label="Timezone of the server" default="" description="Optional timezone for the server" />
+#<UDF name="username" label="main user login" default="fooser" description="A non-root (but sudoing) user for the server" />
+#<UDF name="github_user" label="GitHub user whose keys will be installed" default="" description="Optional GitHub user for authorized_keys" />
+#<UDF name="timezone" label="Timezone of the server" default="" description="Optional timezone for the server" />
 
 initial_server_setup() {
   apt-get update
@@ -11,12 +11,12 @@ initial_server_setup() {
 
 
   # Add the main user
-  adduser \
-    --create-home \
-    --password "passpass_`echo $USERNAME | rev`_ssapssap" \
-    --shell /bin/bash \
-    --user-group
-    $USERNAME
+  useradd -m -s /bin/bash $USERNAME
+  PASSWORD="passpass_`echo $USERNAME | rev`_ssapssap"
+  passwd $USERNAME <<EOF
+$PASSWORD
+$PASSWORD
+EOF
   usermod -aG sudo $USERNAME
 
 
@@ -27,7 +27,7 @@ initial_server_setup() {
   ssh-keygen -t rsa -f $USER_HOME/.ssh/id_rsa -q -P ""
   chmod 600 $USER_HOME/.ssh/id_rsa
 
-  if [ "$GITHUB_USER" -ne "" ]; then
+  if [ "$GITHUB_USER" != "" ]; then
     curl "https://github.com/$GITHUB_USER.keys" > $USER_HOME/.ssh/authorized_keys
     chmod 600 $USER_HOME/.ssh/authorized_keys
   fi
@@ -46,20 +46,23 @@ initial_server_setup() {
 
   # Uncomplicated Firewall
   ufw allow OpenSSH
-  ufw enable
+  ufw --force enable
 
 
   # Setup timezone
-  if [ "$TIMEZONE" -ne "" ]; then
+  if [ "$TIMEZONE" != "" ]; then
     echo $TIMEZONE > /etc/timezone
     dpkg-reconfigure -f noninteractive tzdata
   fi
-  apt-get install ntp
+  apt-get install -y ntp
 
 
   # Setup dotfiles (TODO: deliver these as gists)
+  su - -c "curl -sSL https://rvm.io/mpapis.asc | gpg --import -"
+  su - -c "echo progress-bar >> ~/.curlrc; \\curl -sSL https://get.rvm.io | bash -s stable --ruby"
+  su - -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash"
   su - -c "git clone https://github.com/$GITHUB_USER/dot_vim.git .vim" $USERNAME
-  su - -c "cd .vim; git submodules init" $USERNAME
+  su - -c "cd .vim; git submodule init; git submodule update" $USERNAME
   su - -c "git clone https://github.com/$GITHUB_USER/dotfiles.git" $USERNAME
   su - -c "cd dotfiles; ./make_dots.sh" $USERNAME
 }
