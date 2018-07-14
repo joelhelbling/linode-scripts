8820e74217db5c88a70e50bb270ce28b85ba430b
Joel Helbling <joel@joelhelbling.com>
2018-07-14 10:02:53 -0400

    enhances random_password

--- lib/api/utils.rb
+++ lib/api/utils.rb
@@ -18,9 +18,9 @@ module Api
 
     def random_password
       word_generators = {
-        Faker::Hipster => [:words, 5 + rand(10)],
+        Faker::Hipster => [:words, 5 + rand(10), true],
         Faker::Lovecraft => [:words, 5 + rand(10)],
-        Faker::Lorem => [:words, 5 + rand(10)],
+        Faker::Lorem => [:words, 5 + rand(10), true],
         Faker::NatoPhoneticAlphabet => [:code_word],
         Faker::Food => [:dish],
         Faker::Food => [:ingredient],
@@ -32,23 +32,35 @@ module Api
       words = generators.map do |generator|
         args = word_generators[generator]
         generator.send(*args)
-      end.flatten.sample(2+rand(3))
+      end.flatten.sample(pwd_word_count)
 
-      words.map{|w| maybe_digit(w) }.join(random_delimiter)
+      words \
+        .map{ |w| w.split(' ').sample }
+        .map{ |w| maybe_capitalize(w) }
+        .map{ |w| maybe_digit(w) }
+        .join(random_delimiter)
     end
 
     private
 
+    def pwd_word_count(base=3, range=3)
+      base + rand(range)
+    end
+
     def random_delimiter
-      %w[ + $ ! ? . = - _ # | ].sample
+      (%w[ + $ ! ? . = - _ # | ] << ' ').sample
+    end
+
+    def maybe_capitalize(word)
+      rand(3) == 0 ? word.capitalize : word
     end
 
     def maybe_digit(word)
+      parts = [word]
       if Faker::Boolean.boolean
-        word + rand(9).to_s
-      else
-        word
+        parts << rand(9).to_s
       end
+      parts.shuffle.join
     end
   end
 end
